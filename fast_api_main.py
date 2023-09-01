95b023f7dab4fd6a80780f08687699d39182ac21
rodrigo martinez <rodrigomartinez1302@gmail.com>
2023-08-31 21:49:54 -0300

    coder web test

--- fast_api_main.py
+++ fast_api_main.py
@@ -1,11 +1,19 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
+from pathlib import Path
 
 app = FastAPI()
 
+# Mount the "templates" folder as a static directory
+app.mount("/templates", StaticFiles(directory="templates"), name="templates")
+
 @app.get("/")
 def read_root():
-    return {"message": "Hello, World!"}
+    html_file = Path("templates/index.html")
+    return FileResponse(html_file)
 
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8001)
+
